76824fc8443fcd81d6d11090856ab0e0f44bab8b
uym2 <ceciliamaitouyen@gmail.com>
2018-02-23 16:52:09 -0800

    update interface for user-friendliness

--- simulateTreeFromTopology.py
+++ simulateTreeFromTopology.py
@@ -1,17 +1,30 @@
 #! /usr/bin/env python
 
 from simLib import simulateTreeFromTopology
-from sys import argv
 from dendropy import Tree
+import argparse
+from sys import stdout
 
-infile = argv[1]
-outfile = argv[2]
+parser = argparse.ArgumentParser()
 
-N = 10**6
-alpha = 0
+parser.add_argument("-i","--inputFile",required=True,help="The input tree in Newick format")
+parser.add_argument("-N","--population",required=True,help="Population size")
+parser.add_argument("-g","--growth",required=False,help="Growing rate (exponential) of the population. Default: 0")
+parser.add_argument("-o","--outputFile",required=False,help="The name of the output tree. Default: stdout")
+
+args = vars(parser.parse_args())
+
+infile = args["inputFile"]
+outfile = args["outputFile"] if args["outputFile"] else None
+
+N = float(args["population"])
+alpha = int(args["growth"]) if args["growth"] else 0
 
 myTree = Tree.get_from_path(infile,'newick')
 
 simulateTreeFromTopology(myTree,N,alpha)
 
-myTree.write_to_path(outfile,'newick')
+if outfile is not None:
+    myTree.write_to_path(outfile,'newick')
+else:
+    stdout.write(myTree.as_string('newick'))    
